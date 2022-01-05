ddf0a98e76e4f433b25f82ca7731809fc147269b
GitHub Action <action@github.com>
2022-01-05 13:49:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/indyteo/schoodingplugin/MyBundle.kt
+++ src/main/kotlin/com/github/indyteo/schoodingplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.indyteo.schoodingplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/indyteo/schoodingplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/indyteo/schoodingplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.indyteo.schoodingplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.indyteo.schoodingplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
